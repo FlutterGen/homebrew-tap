e55d317206b03edd377c03bd80f70d907b825ca2
Daichi Furiya <dadadada.chop@gmail.com>
2020-09-05 16:36:32 +0900

    Create fluttergen.rb

---
+++ Formula/fluttergen.rb
@@ -0,0 +1,23 @@
+class FlutterGen < Formula
+  version "v1.0.0"
+  desc "A command-line tool for The Flutter code generator for your assets, fonts, colors, … — Get rid of all String-based APIs. by Dart."
+  homepage "https://github.com/FlutterGen/flutter_gen"
+
+  bottle :unneeded
+
+  if OS.mac?
+    url "https://github.com/FlutterGen/flutter_gen/releases/download/#{version}/fluttergen-macos.tar.gz"
+    sha256 "****************************"
+  elsif OS.linux?
+    url "https://github.com/FlutterGen/flutter_gen/releases/download/#{version}/fluttergen-linux.tar.gz"
+    sha256 "****************************"
+  end
+
+  def install
+    bin.install "fluttergen"
+  end
+
+  test do
+    system "#{bin}/fluttergen", "-h"
+  end
+end
