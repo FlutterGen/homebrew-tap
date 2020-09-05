e1ce0d68704813d75af48a8224b95f02e357495f
Daichi Furiya <dadadada.chop@gmail.com>
2020-09-05 18:33:19 +0900

    Update fluttergen.rb

--- Formula/fluttergen.rb
+++ Formula/fluttergen.rb
@@ -1,16 +1,18 @@
 class FlutterGen < Formula
-  version "v1.0.0"
+  version "v0.0.20"
   desc "A command-line tool for The Flutter code generator for your assets, fonts, colors, … — Get rid of all String-based APIs. by Dart."
   homepage "https://github.com/FlutterGen/flutter_gen"
 
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/FlutterGen/flutter_gen/releases/download/#{version}/fluttergen-macos.tar.gz"
-    sha256 "****************************"
+    url "https://github.com/wasabeef/flutter_gen/releases/download/#{version}/fluttergen-macos.tar.gz"
+    # url "https://github.com/FlutterGen/flutter_gen/releases/download/#{version}/fluttergen-macos.tar.gz"
+    sha256 "7af5c2cf320e7aa188ed787cddeceb1b72f559454b1cb407680c17ab0694218f"
   elsif OS.linux?
-    url "https://github.com/FlutterGen/flutter_gen/releases/download/#{version}/fluttergen-linux.tar.gz"
-    sha256 "****************************"
+    url "https://github.com/wasabeef/flutter_gen/releases/download/#{version}/fluttergen-linux.tar.gz"
+    # url "https://github.com/FlutterGen/flutter_gen/releases/download/#{version}/fluttergen-linux.tar.gz"
+    sha256 "52bedcb37484060171bef4dae666d9535c94caa268eb65fb31a2c1fc9ae2ad29"
   end
 
   def install
