85c969913857edd0254609d22e95722ef2c9a937
Daichi Furiya <dadadada.chop@gmail.com>
2020-09-08 19:49:14 +0900

    Update fluttergen.rb

--- Formula/fluttergen.rb
+++ Formula/fluttergen.rb
@@ -6,10 +6,10 @@ class Fluttergen < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/wasabeef/flutter_gen/releases/download/#{version}/fluttergen-macos.tar.gz"
+    url "https://github.com/FlutterGen/flutter_gen/releases/download/#{version}/fluttergen-macos.tar.gz"
     sha256 "23815e7343e4d8f68803c11a2a6f3ebe79c401ab2b6bd4279a34c40e58a8568a"
   elsif OS.linux?
-    url "https://github.com/wasabeef/flutter_gen/releases/download/#{version}/fluttergen-linux.tar.gz"
+    url "https://github.com/FlutterGen/flutter_gen/releases/download/#{version}/fluttergen-linux.tar.gz"
     sha256 "15705952b5f0cff9e49a140dbd298bbff427d39670cca032fa27ba169648e855"
   end
 
