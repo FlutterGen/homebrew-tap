29bd31c33b41e801ef09b42eb128a854315d5530
Daichi Furiya <dadadada.chop@gmail.com>
2022-05-25 18:41:24 +0900

    v4.2.0

--- Formula/fluttergen.rb
+++ Formula/fluttergen.rb
@@ -1,14 +1,14 @@
 class Fluttergen < Formula
-  version "v4.1.6"
+  version "v4.2.0"
   desc "A command-line tool for The Flutter code generator for your assets, fonts, colors, … — Get rid of all String-based APIs. by Dart."
   homepage "https://github.com/FlutterGen/flutter_gen"
 
   if OS.mac?
     url "https://github.com/FlutterGen/flutter_gen/releases/download/#{version}/fluttergen-macos.tar.gz"
-    sha256 "a83d283440fc90fbb828f1ace7be60bdb61a265c6b8b408738e8419b3d460b3d"
+    sha256 "1a12734537f216efb2e695945014c5d9781c9f5d47e8247dac87084f77ecf445"
   elsif OS.linux?
     url "https://github.com/FlutterGen/flutter_gen/releases/download/#{version}/fluttergen-linux.tar.gz"
-    sha256 "05256e267c442d44b803906522098d4003ff4584c339fe29483d5a2c46cda861"
+    sha256 "27f5be8cf95b757191ffd3fae0b6f48af3decb440e2a187791f80ec70b9f8d5f"
   end
 
   def install
